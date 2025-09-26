3e19e4a872d628cbb4d48a710e81fcbe7158a20c
gitgeorge170211 <andgeorge20@gmail.com>
2025-09-26 09:53:33 +0300

    background tiles

--- code/main.py
+++ code/main.py
@@ -26,6 +26,10 @@ class Game():
 
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
+
+        for x, y, image in map.get_layer_by_name("Ground").tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
         for obj in map.get_layer_by_name("Objects"):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
 
--- code/sprites.py
+++ code/sprites.py
@@ -1,7 +1,13 @@
 from settings import *
 
-class CollisionSprite(pygame.sprite.Sprite):
+class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(center = pos)
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(topleft = pos)
