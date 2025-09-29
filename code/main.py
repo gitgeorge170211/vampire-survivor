af39fa8799d31ec21238061c9ccdd049a19a0b19
gitgeorge170211 <andgeorge20@gmail.com>
2025-09-29 12:38:27 +0300

    collision blocks adition

--- code/main.py
+++ code/main.py
@@ -27,11 +27,16 @@ class Game():
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
 
+
         for x, y, image in map.get_layer_by_name("Ground").tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
         for obj in map.get_layer_by_name("Objects"):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name("Collisions"):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.collision_sprites))
+            
 
     def run(self):
         while self.running:
