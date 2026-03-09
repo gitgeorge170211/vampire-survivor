c81489fa39ac47d668b3a259d93730b3424a8249
gitgeorge170211 <andgeorge20@gmail.com>
2026-03-09 11:38:06 +0200

    τεχνολογία 9/3 v1

--- code/test.py
+++ code/test.py
@@ -1,23 +1,42 @@
 import pygame
+from math import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
+
+def get_angle(hand_pos):
+    mouse_pos = pygame.mouse.get_pos()
+
+    dx = (mouse_pos[0] - hand_pos[0])
+    dy = (mouse_pos[1] - hand_pos[1])
+    mouse_angle = atan2(-dy, dx)
+    mouse_angle = degrees(mouse_angle)
+
+    return mouse_angle
+
 color = (200, 100, 70)
-surface = pygame.Surface((30, 50))
-surface.fill(color)
-surface.set_alpha(145)
+original_surface = pygame.Surface((20, 100), pygame.SRCALPHA)
+original_surface.fill(color)
+surface = original_surface.copy()
+rect = original_surface.get_frect(center = (250, 250))
+# surface.set_alpha(145)
 running = True
 while running:
-    clock.tick(5)
+    clock.tick(20)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+
+    mouse_angle = get_angle((250, 250))
+    surface = pygame.transform.rotate(original_surface, mouse_angle + 90)
+    rect = surface.get_frect(center = (250, 250))
+
     screen.fill((0,0,0))
-    screen.blit(surface, (100, 100))
+    screen.blit(surface, rect)
     pygame.display.update()
 
 pygame.quit()
